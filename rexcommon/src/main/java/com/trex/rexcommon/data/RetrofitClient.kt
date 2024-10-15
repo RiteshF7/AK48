87b9e9984037c8caa61c0a98294ce4dd4971fc51
ritesh <riteshf7@gmail.com>
2024-10-15 16:47:36 +0530

    added firebase fcm working set

--- rexcommon/src/main/java/com/trex/rexcommon/data/RetrofitClient.kt
+++ rexcommon/src/main/java/com/trex/rexcommon/data/RetrofitClient.kt
@@ -20,8 +20,8 @@ interface RexKtorServer {
 }
 
 object RetrofitClient {
-    //    private const val BASE_URL = "http://10.0.2.2:8080/"
-    private const val BASE_URL = "http://192.168.0.165:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
+//    private const val BASE_URL = "http://192.168.0.165:8080/"
 
     val builder: RexKtorServer =
         Retrofit
